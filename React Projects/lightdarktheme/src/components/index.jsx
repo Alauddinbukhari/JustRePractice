890268c59b89e43e58255ca0b9be5ceff7f62b26
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2025-07-24 21:08:37 -0600

    done some react project.

---
+++ React Projects/lightdarktheme/src/components/index.jsx
@@ -0,0 +1,20 @@
+import useLocalStorage from "./useLocalStorage"
+
+
+
+
+export default function lightDarkMode(){
+
+const [theme, setTheme] = useLocalStorage("theme", "light");
+
+
+function handleToggleTheme() {
+    setTheme(theme === "light" ? 'dark': 'light');
+
+    return <div className="light-dark-mode">
+        <div className="container">
+            <p>Hello World!</p>
+            <button onClick={handleToggleTheme}>Change Theme</button>
+        </div>
+    </div>
+}
