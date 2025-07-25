54890034956f8dee5a5d0542ed096c1075ca3475
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2025-07-24 22:14:36 -0600

    Completed react-project - lightdarkmode

--- React Projects/lightdarktheme/src/App.jsx
+++ React Projects/lightdarktheme/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-
+import LightDarkMode from './components/index.jsx'
 function App() {
  
 
   return (
-    <>
-   
-    </>
+    <div className="App">
+      <h1>Light/Dark Theme Toggle</h1>
+      <p>Click the button below to toggle between light and dark themes.</p>
+    <LightDarkMode />
+    </div>
   )
 }
 
--- React Projects/lightdarktheme/src/components/index.jsx
+++ React Projects/lightdarktheme/src/components/index.jsx
@@ -3,14 +3,14 @@ import useLocalStorage from "./useLocalStorage"
 
 
 
-export default function lightDarkMode(){
+export default function LightDarkMode(){
 
 const [theme, setTheme] = useLocalStorage("theme", "light");
 
 
 function handleToggleTheme() {
     setTheme(theme === "light" ? 'dark': 'light');
-
+}
     return <div className="light-dark-mode">
         <div className="container">
             <p>Hello World!</p>
