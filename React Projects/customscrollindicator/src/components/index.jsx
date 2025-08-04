63ad93217cfa47c0e2ba1c5e18812f0a3b7a021b
Syed Alauddin Bukhari <syedalauddin.b@gmail.com>
2025-08-03 19:37:23 -0600

    Makes changes in react app

--- React Projects/customscrollindicator/src/components/index.jsx
+++ React Projects/customscrollindicator/src/components/index.jsx
@@ -7,6 +7,7 @@ export default function ScrollIndicator({ url }) {
     const [data, SetData] = useState(null);
     const [loading, SetLoading] = useState(false);
     const [error, SetError] = useState(null);
+    const [scrollPercentage, setScrollPercentage] = useState(0);
 
     async function fetchData(getUrl) {
         try {
@@ -31,14 +32,44 @@ export default function ScrollIndicator({ url }) {
         fetch(url);
 
     }, [url])
+
+
+    function handleScrollPercentage() {
+        console.log(document.body.scrollTop, document.documentElement.scrollTop, document.documentElement.scrollHeight, document.documentElement.clientHeight);
+        const howMuchScrolled = document.body.scrollTop || document.documentElement.scrollTop;
+
+
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        setScrollPercentage((howMuchScrolled / height) * 100);
+    }
+
+
+    useEffect(() => {
+
+        window.addEventListener('scroll', handleScrollPercentage)
+        return () => {
+            window.removeEventListener('scroll', handleScrollPercentage);
+        }
+    }, [])
+
+
+
     console.log("Data:", data, loading);
 
-    return <div><h1>Custom Scroll Indicator</h1>
+    return <div>
+        <div className="top-container">
+
+            <h1>Custom Scroll Indicator</h1>
+            <div className="scroll-progress-tracking-container"></div>
+            <div className="current-progress-bar" style={{ width: `${scrollPercentage}` }}></div>
+        </div>
+
+
         <div className="data-container">
 
             {data && data.length > 0 ?
                 data.map(dataItem => <p> {dataItem.title}</p>)
-            :null}
+                : null}
         </div>
 
 
